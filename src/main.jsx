707726feb60bb6a079b1d63169515b86d746b32a
High23 <warenasb2@gmail.com>
2024-03-13 23:25:52 -0700

    Added more routes

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,24 @@ import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import App from './App.jsx'
 import './index.css'
+import { ShopCategoriesNavigation } from './components/shopping/shop-page.jsx'
 
 
 const router = createBrowserRouter([
   {
     path: "/*",
+    element: <App></App>,
+  },
+  {
+    path: "/:directory",
+    element: <App></App>,
+  },
+  {
+    path: "/:directory/:category",
+    element: <App></App>,
+  },
+  {
+    path: "/:directory/:category/:type",
     element: <App></App>,
   },
 ])
