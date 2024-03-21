7d2a57ce014db0881c2024d551fe33d6ad164593
High23 <warenasb2@gmail.com>
2024-03-20 22:34:48 -0700

    Added cart component and more props to components

--- src/App.jsx
+++ src/App.jsx
@@ -3,23 +3,27 @@ import styles from './App.module.css'
 import NavigationBar from './components/navigation-bar/navigation-bar'
 import { useParams } from 'react-router-dom'
 import ShowHomePage from './components/home-page/home-page'
-import { ShopCategoriesNavigation, ShopCategories, ShowCategories, MensClothing, WomensClothing } from './components/shopping/shop-page'
+import { ShopCategoriesNavigation, ShopCategories} from './components/shopping/shop-page.jsx'
+import ShoppingCart from './components/shopping/shopping-cart'
 
 
 function App() {
     const { directory } = useParams()
-    const {category} = useParams()
-    const {type} = useParams()
+    const { category } = useParams()
+    const { type } = useParams()
+    const [cart, setCart] = useState([])
     return (
         <>
             <header>Shopping cart Project</header>
-            <NavigationBar></NavigationBar>
+            <NavigationBar cart={cart}></NavigationBar>
             <main>
                 {directory === 'shop' ?
                 <ShopCategoriesNavigation category={category}></ShopCategoriesNavigation> :
+                directory === 'cart' ?
+                <ShoppingCart cart={cart}></ShoppingCart> :
                 <ShowHomePage></ShowHomePage>
                 }
-                <ShopCategories category={category} type={type}></ShopCategories>
+                <ShopCategories category={category} type={type} cart={cart} setCart={setCart}></ShopCategories>
             </main>
         </>
     )
