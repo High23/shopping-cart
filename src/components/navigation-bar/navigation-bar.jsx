e8c4c7b7da2bf5f526bb537e3fb2d0d7669e61e1
High23 <warenasb2@gmail.com>
2024-03-20 22:35:55 -0700

    Added cart option to navigation bar

--- src/components/navigation-bar/navigation-bar.jsx
+++ src/components/navigation-bar/navigation-bar.jsx
@@ -1,11 +1,12 @@
 import { Link } from "react-router-dom"
 import styles from "./navigation-bar.module.css"
 
-function NavigationBar() {
+function NavigationBar({cart}) {
     return (
         <ul>
             <li><Link to='/'>Home</Link></li>
             <li><Link to="/shop">Shop</Link></li>
+            <li><Link to="/cart">Cart</Link></li>
         </ul>
     )
 }
