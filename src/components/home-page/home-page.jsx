c442d0db0d646e3947b5f99fe8197e430144cbdc
High23 <warenasb2@gmail.com>
2024-03-23 00:00:38 -0700

    Added title, paragraph and shop button

--- src/components/home-page/home-page.jsx
+++ src/components/home-page/home-page.jsx
@@ -1,7 +1,13 @@
+import { Link } from "react-router-dom"
+import styled from "../navigation-bar/navigation-bar.module.css"
+
+
 function ShowHomePage() {
     return (
         <>
-
+            <h1>The Modern Shop</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas non quam non nibh elementum tincidunt eget et ipsum. Ut tristique turpis ac nulla pharetra condimentum.</p>
+            <Link to="/shop" ><button className={styled.navigationBarButton}>Shop</button></Link>
         </>
     )
 }
