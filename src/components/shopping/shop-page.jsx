251d866b8beb74f8ad502623f8f5242834ad389c
High23 <warenasb2@gmail.com>
2024-03-26 01:27:38 -0700

    Adjusted navbar to include men and women clothing

--- src/components/shopping/shop-page.jsx
+++ src/components/shopping/shop-page.jsx
@@ -1,35 +1,35 @@
 import { useEffect, useState } from "react"
-import { useParams, Link } from "react-router-dom"
+import { Link } from "react-router-dom"
 import  styles  from './shop-page.module.css'
 import { addItemsToCart, subtractOneFromQuantity, addOneToQuantity, getCategoryData } from "./shop-page.js"
 
-function ShopCategoriesNavigation({category}) {
+function ShopCategoriesNavigation() {
     return (
         <ul className={styles.shopCategoryNavigationBar}>
-            <li><Link to='/shop/electronics'>Electronics</Link></li>
-            <li><Link to='/shop/jewelery'>Jewelry</Link></li>
-            <li><Link to='/shop/clothing'>Clothing</Link></li>
-            {category === 'clothing' && <ClothingNavigation></ClothingNavigation>}
+            <li><Link to='/shop/electronics' className={styles.shopNavigationBarButton}>Electronics</Link></li>
+            <li><Link to='/shop/jewelery' className={styles.shopNavigationBarButton}>Jewelry</Link></li>
+            <li><Link to='/shop/mens-clothing' className={styles.shopNavigationBarButton}>Men&apos;s Clothing</Link></li>
+            <li><Link to='/shop/womens-clothing' className={styles.shopNavigationBarButton}>Women&apos;s Clothing</Link></li>
         </ul>
     )
 }
 
-function ShopCategories({category, type, cart, setCart}) {
+function ShopCategories({category, cart, setCart}) {
     const [items, setItems] = useState(null)
     const [error, setError] = useState(null)
     const [loading, setLoading] = useState(true)
     useEffect(() =>  {
-        if (category === 'clothing' && type === 'men') {
+        if (category === 'mens-clothing') {
             getCategoryData(setItems, setError, setLoading, "men's clothing")
             return
-        } else if (category === 'clothing' && type === 'women') {
+        } else if (category === 'womens-clothing') {
             getCategoryData(setItems, setError, setLoading, "women's clothing")
             return
-        } else if (category === 'clothing' && type === undefined){
+        } else if (category === undefined){
             return
         }
         getCategoryData(setItems, setError, setLoading, category)
-    }, [category, type])
+    }, [category])
 
     return (
         <>
@@ -44,6 +44,7 @@ function CategoryItems({fetchedItems, loading, cart, setCart}) {
             {!loading && 
             <section className={styles.products} aria-label="productContainer">
                 {fetchedItems.map((item, index) => {
+                    item = {...item, price: Math.ceil(item.price)}
                     return (
                         <div key={item.id} className={styles.productContainer}>
                             <span><img src={item.image} alt="" className={styles.productImage}></img></span>
@@ -58,32 +59,22 @@ function CategoryItems({fetchedItems, loading, cart, setCart}) {
                                         <button type="button" onClick={() => {
                                             subtractOneFromQuantity(index)
                                         }}>-</button>
-                                        <input type="number" name="product quantity" id="quantity-of-selected-product" defaultValue={0}></input>
+                                        <input type="number" name="product quantity" id="quantity-of-selected-product" defaultValue={1}></input>
                                         <button type="button" onClick={() => {
                                             addOneToQuantity(index)
                                         }}>+</button>
                                     </span>
-                                    <button type="button"  onClick={() => {
+                                    <button type="button" onClick={() => {
                                         addItemsToCart(item, index, cart, setCart)
                                     }}>Add to cart</button>
                                 </form>
                             </div>
-                            
                         </div>
                     )
                 })}
             </section>
             }
         </>
-    )
-}
-
-function ClothingNavigation() {
-    return (
-        <ul>
-            <li><Link to='/shop/clothing/men'>Men&apos;s Clothing</Link></li>
-            <li><Link to='/shop/clothing/women'>Women&apos;s clothing</Link></li>
-        </ul>
     )
 }
 
