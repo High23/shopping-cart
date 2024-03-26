94526c520eaa979f286446a674ee1b9324a690b1
High23 <warenasb2@gmail.com>
2024-03-26 01:23:59 -0700

    Removed an import

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import App from './App.jsx'
 import './index.css'
-import { ShopCategoriesNavigation } from './components/shopping/shop-page.jsx'
-
 
 const router = createBrowserRouter([
   {
